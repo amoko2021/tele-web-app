38ec84d16a5a497e9bbc31dfcf4e507fa757a4c4
small <83492837+amoko2021@users.noreply.github.com>
2026-01-05 10:48:26 +0700

    Comment out requestFullscreen in useTelegram hook
    Comment out the requestFullscreen call in useTelegram hook.

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -10,7 +10,7 @@ export const useTelegram = () => {
   useEffect(() => {
     tg?.ready()
     tg?.expand()
-    tg?.requestFullscreen()
+    //tg?.requestFullscreen()
 
     // Validate init data khi component mount
     validateInitData()
