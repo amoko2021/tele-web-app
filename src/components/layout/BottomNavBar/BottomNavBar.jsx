b0648040d7baf2e6d029fb494a5b203ffd1cbb46
small <83492837+amoko2021@users.noreply.github.com>
2026-01-05 10:39:08 +0700

    Update label for home navigation item

--- src/components/layout/BottomNavBar/BottomNavBar.jsx
+++ src/components/layout/BottomNavBar/BottomNavBar.jsx
@@ -4,7 +4,7 @@ import styles from './BottomNavBar.module.css'
 
 const navItems = [
   { path: '/account', icon: User, label: 'Tài khoản' },
-  { path: '/', icon: Home, label: 'Home' },
+  { path: '/', icon: Home, label: 'Trnag chu' },
   { path: '/settings', icon: Navigation, label: 'Mời bạn bè' },
 ]
 
