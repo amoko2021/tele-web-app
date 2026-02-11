f776a9a23d4e9647dc6cc5369cdefbb31a26b8c2
Duck <tuananh260501@gmail.com>
2026-02-11 10:57:05 +0700

    update: hide play & survey task

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -343,14 +343,14 @@ export const Home = () => {
           gradientColors="from-purple-500 to-pink-600"
         /> */}
 
-        <TaskButton
+        {/* <TaskButton
           onClick={() => navigate('/flappy-bird')}
           text={UI_TEXT.flappyBird.title}
           amount="Play & Earn"
           gradientColors="from-green-500 to-emerald-600"
         />
 
-        <TaskButton onClick={() => navigate('/test')} />
+        <TaskButton onClick={() => navigate('/test')} /> */}
 
         {/* Task Button */}
         {/* <TaskButton onClick={() => navigate('/test')} /> */}
