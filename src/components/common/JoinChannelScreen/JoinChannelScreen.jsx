ef6a52d1952d739d8c0a622aa0c313115fc4182e
Duck <tuananh260501@gmail.com>
2026-03-13 11:00:07 +0700

    feat: add payment proof channel button to JoinChannelScreen

--- src/components/common/JoinChannelScreen/JoinChannelScreen.jsx
+++ src/components/common/JoinChannelScreen/JoinChannelScreen.jsx
@@ -5,13 +5,22 @@ import styles from './JoinChannelScreen.module.css'
 export const JoinChannelScreen = ({ onCheck }) => {
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const channelUrl = 'https://t.me/lamgiau_online' // Placeholder
+  const channelUrl = 'https://t.me/lamgiau_online'
+  const sub_channelUrl = 'https://t.me/trathuongonline'  
 
   const handleJoin = () => {
     if (window.Telegram?.WebApp?.openTelegramLink) {
       window.Telegram.WebApp.openTelegramLink(channelUrl)
     } else {
       window.open(channelUrl, '_blank')
+    }
+  }
+
+  const handleSubJoin = () => {
+    if (window.Telegram?.WebApp?.openTelegramLink) {
+      window.Telegram.WebApp.openTelegramLink(sub_channelUrl)
+    } else {
+      window.open(sub_channelUrl, '_blank')
     }
   }
 
@@ -44,6 +53,9 @@ export const JoinChannelScreen = ({ onCheck }) => {
       <div className={styles.buttonGroup}>
         <button className={styles.joinButton} onClick={handleJoin}>
           {UI_TEXT.joinChannelScreen.button}
+        </button>
+        <button className={styles.subButton} onClick={handleSubJoin}>
+          {UI_TEXT.joinChannelScreen.proofButton}
         </button>
         <button 
           className={styles.checkButton} 
